b54856842c7f57cd01a129ecb8987ab6b9d5a186
adanisantana <mariano.dani@hotmail.com>
2025-01-13 13:22:26 -0300

    froentend componente home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <div  className= "margem">
+        <div className='alinhamento'>
+            <p>Encontre Desenvolvedores que compartilham o que sabem, se desenvolva conosco.</p>
+            
+        <div className="imagem">
+        <img 
+            src="./src/assets/bemVindo.png" 
+            alt="Imagem da Página Home" 
+            width="1300px"/>
+        </div>
+    </div>
+</div>
+    </>
+  )
+}
+
+export default Home
